1c2865cae2fddc69b470d1308275b2d76185ddb4
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-08-02 18:13:59 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: 'http://localhost:5173',
-    },
+    cors: true
   });
-  console.log({ x: process.env.DB_CONN_STRING });
-  await app.listen(3000);
 }
 bootstrap();
