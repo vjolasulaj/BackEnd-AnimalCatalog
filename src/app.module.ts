7cc9ba60ec5ceff2a3e4bb085b91b1c309498f09
Vjola Sulaj <sulaj.vjolaa@gmail.com>
2024-08-02 17:16:25 +0200

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,26 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
 import { DogsModule } from './dogs/dogs.module';
 import { BirdsModule } from './birds/birds.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from 'config/configuration';
 
 @Module({
-  imports: [AnimalsModule, MongooseModule.forRoot('mongodb://localhost:27017/nest'), CatsModule, DogsModule, BirdsModule], 
+  imports: [
+    AnimalsModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.getOrThrow<string>('database.connection_string'),
+        };
+      },
+    }),
+    CatsModule,
+    DogsModule,
+    BirdsModule,
+    ConfigModule.forRoot({ load: [configuration] }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
